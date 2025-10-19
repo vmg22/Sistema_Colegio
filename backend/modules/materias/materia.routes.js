5d04665f684610d567f48d1a94ce580418dc892e
juan marcelo <juanmarcelo84@gmail.com>
2025-10-19 12:51:47 -0300

    se modifica modulo de controller materia con query y nombres en español

--- backend/modules/materias/materia.routes.js
+++ backend/modules/materias/materia.routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const materiaController = require('./materia.controller');
 
-router.get('/', materiaController.getAllMaterias);
-router.get('/eliminados/listar', materiaController.getMateriasEliminadas);
-router.get('/:id', materiaController.getMateriaById);
-router.post('/', materiaController.createMateria);
+router.get('/', materiaController.obtenerTodasMaterias);
+router.get('/eliminados/listar', materiaController.obtenerMateriasEliminadas);
+router.get('/:id', materiaController.obtenerMateriaPorId);
+router.post('/', materiaController.crearMateria);
 router.post('/:id/restaurar', materiaController.restaurarMateria);
-router.put('/:id', materiaController.updateMateria);
-router.delete('/:id', materiaController.deleteMateria);
+router.put('/:id', materiaController.actualizarMateria);
+router.delete('/:id', materiaController.eliminarMateria);
 
 module.exports = router;
