260d9fe813aef9d6908edde422c0444303a79f17
Matias Garnica <victormatiasgarnica@gmail.com>
2025-10-21 22:21:33 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import './styles/index.css'
 import ButtonShowcase from './components/ui/Botones'
 import Dashboard from './pages/dashboard/Dashboard'
 import Header from './components/layout/Header'
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
-import { DocenteProvider } from './context/DocenteContext';
 
 import { Admin } from './pages/admin/Admin';
 
@@ -16,20 +14,6 @@ function App() {
       <ButtonShowcase/>
       <Dashboard/>
  
-      <DocenteProvider>
-
-          <BrowserRouter>
-            <Routes>
-              
-              {/* 4. Configura tu página de Admin como la ruta principal ("/") */}
-              <Route path="/" element={<Admin />} />
-
-              {/* Puedes añadir otras rutas de prueba aquí si lo necesitas */}
-              <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
-            
-            </Routes>
-          </BrowserRouter>
-      </DocenteProvider>
 
       <h1>hola matias</h1>
       
